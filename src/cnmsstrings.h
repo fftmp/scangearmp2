900b9b26d3d435437c6252a60b0bebf0acdb857a
fft <anonim288@gmail.com>
2017-09-05 18:53:47 +0300

    move gettext macros

--- src/cnmsstrings.h
+++ src/cnmsstrings.h
@@ -27,8 +27,29 @@
 #ifndef	_CNMSSTRINGS_H_
 #define	_CNMSSTRINGS_H_
 
-/* N_ macro */
-#include "support.h"
+/*
+        Standard gettext macros.
+*/
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#  undef _
+#  define _(String) dgettext (PACKAGE, String)
+#  define Q_(String) g_strip_context ((String), gettext (String))
+#  ifdef gettext_noop
+#    define N_(String) gettext_noop (String)
+#  else
+#    define N_(String) (String)
+#  endif
+#else
+#  define textdomain(String) (String)
+#  define gettext(String) (String)
+#  define dgettext(Domain,Message) (Message)
+#  define dcgettext(Domain,Message,Type) (Message)
+#  define bindtextdomain(Domain,Directory) (Domain)
+#  define _(String) (String)
+#  define Q_(String) g_strip_context ((String), (String))
+#  define N_(String) (String)
+#endif
 
 #define	STR_CNMS_LS_007_VER_NUM		"3.50"
 #define	STR_CNMS_LS_007_YEAR_BEGIN	"2007"
